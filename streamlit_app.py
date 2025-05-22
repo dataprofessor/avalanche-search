314ba99c4eb70aa40499698178ac8171e5f2790e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-05-21 21:49:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title("❄️ Using Snowflake in Streamlit Community Cloud")
 
-st.write('Hello world!')
+# Connect to Snowflake
+conn = st.connection("snowflake")
+prompt = st.text_input('What do you want to know?', placeholder='Ask a question')
+  
+if st.button("Submit", type='primary'):
+  response = conn.query(f"SELECT SNOWFLAKE.CORTEX.COMPLETE('claude-3-5-sonnet', '{prompt}') as RESPONSE;")
+  response_value = response.loc[0, 'RESPONSE']
+  st.code(response_value, language=None)
