3228942388f3f07fb0ee5b5157dbc3ff0a0a71bb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-05-21 22:06:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -221,3 +221,6 @@ if __name__ == "__main__":
     # session = st.connection("snowflake")
     root = Root(session)
     main()
+
+search_service = session.sql("SHOW CORTEX SEARCH SERVICES;")
+st.write(search_service)
